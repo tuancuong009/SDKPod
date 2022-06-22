07a5e2f5e5c27ff0d9e03f60ee4c4701f2ac621f
QTS Coder <tester.qts@gmail.com>
2022-06-22 14:53:10 +0700

    Initial project setup

---
+++ SwiftyLib/SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by QTS Coder on 22/06/2022.
+//
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
